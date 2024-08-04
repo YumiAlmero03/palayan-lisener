e2a550c8b39bb4c25e662a00c2d812ece0a1ceee
Lanie Almero <lanialmero@gmail.com>
2024-08-05 02:09:35 +0800

    cleanup 2

--- dtr/config.php
+++ dtr/config.php
@@ -17,7 +17,7 @@ $sleep = 1;
 define('SMS_URL', 'http://127.0.0.1:8001/api/received-attendance');
 define('DTR_URL', 'http://127.0.0.1:8001/api/last-attendance');
 
-define('LOCAL_URL', 'http://127.0.0.1:8000/api/received-attendance');
+define('LOCAL_URL', 'http://127.0.0.1:8001/api/received-attendance');
 
 
 /*120.28.86.165*/
